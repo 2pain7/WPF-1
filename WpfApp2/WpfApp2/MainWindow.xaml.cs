6914edafe559bc318d32e066cd5705ab29315c1a
2pain7 <dimitriepesic@gmail.com>
2024-10-11 11:14:23 +0500

    Design

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -26,37 +26,6 @@ namespace WpfApp2
             
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show ("Кнопка зажата");
-        }
-
-        private void Check_Checked(object sender, RoutedEventArgs e)
-        {
-            if (Check.IsChecked == true)
-                MessageBox.Show("Is checked == true");
-        }
-
-        private void ZD_Checked(object sender, RoutedEventArgs e)
-        {
-
-            if (ZD.IsChecked == true)
-
-                MessageBox.Show("with ZD");
-        }
-
-        private void DVD_Checked(object sender, RoutedEventArgs e)
-        {
-            if (DVD.IsChecked == true)
-
-                MessageBox.Show("with DVD");
-        }
-
-        private void OS_Checked(object sender, RoutedEventArgs e)
-        {
-            if (OS.IsChecked == true)
-
-                MessageBox.Show("for OS");
-        }
+       
     }
 }
